2122466d6c79f4d16d389c9e2ded4bdb7b79eed3
Bluewave2 <83724034+Bluewave2@users.noreply.github.com>
2024-05-20 23:04:35 +0200

    Updated privilege escalation script, keybinds

--- main/HyperVDisableEnable-v1.0.0.cmd
+++ main/HyperVDisableEnable-v1.0.0.cmd
@@ -1,29 +1,24 @@
 @ECHO OFF
 @setlocal DisableDelayedExpansion
-%1 %2
-ver|find "5.">nul&& goto :Admin
-mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof
-:Admin
-
-goto check_Permissions
-
-:check_Permissions
-    echo Administrative permissions required. Detecting permissions...
-
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        cls
-::        echo Success: Administrative permissions confirmed.
-::        timeout 2
-        goto P_start
-    ) else (
-        cls
-        echo Failure: Current permissions inadequate. Please relaunch as Administrator
-        pause >nul
-    )
-
+REM --------------BEGINNING OF SCRIPT--------------
+REM
+REM This part is a version of https://superuser.com/a/852877 (thanks)
+REM
+REM  --> Check for permissions  
+net session>nul 2>&1 
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (    echo Requesting administrative privileges...    goto UACPrompt) else ( goto gotAdmin )  
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"  
+    "%temp%\getadmin.vbs"  
+    exit /B
+:gotAdmin
+	REM cls
+	REM echo Success: Administrative permissions confirmed.
+	REM timeout 1
 :P_start
-set vers=v0.1
+set vers=v1.0.0
 title HyperVDisableEnable %vers%
 cls
 for /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
@@ -36,22 +31,22 @@ ECHO: Windows Build: %winbuild%
 ECHO:
 ECHO: Status:
 ECHO:-----------------------------
-bcdedit /enum | find /I "hypervisorlaunchtype"
+%bcdedit /enum | find /I "hypervisorlaunchtype"%
 ECHO:-----------------------------
 ECHO:
-ECHO: 2. Disable
+ECHO: 1. Disable
 ECHO:
-ECHO: 3. Enable
+ECHO: 2. Enable
 ECHO:
-ECHO: 5. Exit
+ECHO: 3. Readme
 ECHO:
-ECHO: 7. Readme
+ECHO: 4. Exit
 ECHO:
 ECHO:===========================================================
 ECHO:
-choice /C:2357 /N /M ">Select an option with your Keyboard [2,3,5,7] : "
-if errorlevel  4 goto:Readme
-if errorlevel  3 goto:Exitnt
+choice /C:1234 /N /M ">Select an option with your Keyboard [1,2,3,4] : "
+if errorlevel  4 goto:Exitnt
+if errorlevel  3 goto:Readme
 if errorlevel  2 goto:Enable
 if errorlevel  1 goto:Disable
 pause
@@ -111,20 +106,23 @@ ECHO: https://github.com/Bluewave2/HyperV-Disable-Enable
 ECHO:----------------------------------------------------------
 ECHO:                 -- Licenced GNU GPLv3 --
 ECHO:
-ECHO: A script made for easily disabling and enabling
-ECHO: Hyper-V for usage of virtualization products
-ECHO: without uninstalling features in Windows 10.
+ECHO: A script made for easily disabling and enabling Hyper-V
+ECHO: for usage of virtualization products without uninstalling
+ECHO: features in Windows 10.
 ECHO: Feel free to contribute on the Github page.
 ECHO:
-ECHO: 5. Exit
+ECHO: 1. Open GitHub repository [link]
 ECHO:
-ECHO: 7. Main Menu
+ECHO: 4. Main Menu
 ECHO:
 ECHO:===========================================================
 ECHO:
-choice /C:75 /N /M ">Select an option with your Keyboard [5,7] : "
-if errorlevel  2 goto:Exitnt
-if errorlevel  1 goto:P_start
+choice /C:14 /N /M ">Select an option with your Keyboard [1,4] : "
+if errorlevel  2 goto:P_start
+if errorlevel  1 goto:OpenGitHub
+
+:OpenGitHub
+start "" https://github.com/Bluewave2/HyperV-Disable-Enable
 
 :Restart
 ECHO Operation complete, restart required to finish.
@@ -143,29 +141,10 @@ ECHO:
     else (
         cls
         shutdown /a
-        Echo: You chose not to restart now. Restart later to apply changes.
+        echo: You chose not to restart now. Restart later to apply changes.
+		echo:
+		echo: Thank you for using this script, have a nice day :)
         timeout 7
         goto:P_start
     )
-@REM :Restart
-@REM ECHO Operation complete, restart required to finish.
-@REM ECHO:
-@REM     set /p Input=Type y to confirm:
-@REM     if %Input% == y (
-@REM         cls
-@REM         ECHO Restart initiated, you have 10 seconds to abort
-@REM         shutdown /r /t 10 /c "HyperVDisableEnable restart procedure"
-@REM         set /p Input=Type a to abort:
-@REM         if %Input% == a (
-@REM             shutdown /a
-@REM             ECHO Restart aborted.
-@REM             ECHO:
-@REM             ECHO You can manually restart your PC to apply the changes.
-@REM             ECHO:
-@REM             ECHO:
-@REM             ECHO This program will now terminate.
-@REM             ECHO Thank you for using this program and have a nice day :)
-@REM             pause
-@REM         )
-@REM     )
-@REM )
+REM --------------END OF SCRIPT--------------
